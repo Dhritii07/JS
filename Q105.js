ca44e8b197af965ebc2cb4adfc42fbd2c011c42a
Dhritii07 <dhritivaidya11@gmail.com>
2025-01-08 18:00:20 +0530

    initial commit

---
+++ Q105.js
@@ -0,0 +1,12 @@
+// Check if Value is a Number
+
+function isNum(num){
+    if (typeof num ==='number'){
+        return true
+    }
+    else{
+        return false
+    }
+}
+
+console.log(isNum(89))
