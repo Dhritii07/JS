d5eabb11e881219b08edfc53e72f66e636841621
Dhritii07 <dhritivaidya11@gmail.com>
2025-01-09 16:06:48 +0530

    initial commit

--- Q8.js
+++ Q8.js
@@ -2,5 +2,7 @@
 const nums = [10,2,3,4,5,6,8,7]
 const max = 5
 
-const filtered = nums.filter(num => num <= max);
+const filtered = nums.filter(
+    num => 
+        num <= max);
 console.log(filtered)
