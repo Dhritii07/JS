2d939fa22d5e59aed51ed23275fd5180e9fce36b
Dhritii07 <dhritivaidya11@gmail.com>
2025-01-10 11:18:39 +0530

    initial commit

---
+++ slice/1to4.js
@@ -0,0 +1,19 @@
+// Extract the substring "world" from the string "Hello, world!" using the slice method.
+str = "Hello, world!"
+str1 = str.slice(7,12)
+console.log(str1)
+
+// Use the slice method to extract the last 4 characters from the string "JavaScript".
+str = "JavaScript"
+str1 = str.slice(6,11)
+console.log(str1)
+
+// Given the string "Coding is fun!", extract the word "Coding" using the slice method.
+str = "Coding is fun!"
+str1 = str.slice(0,6)
+console.log(str1)
+
+//Use the slice method to remove the first 5 characters from the string "Hello, there!".
+str = "Hello, there!"
+str1 = str.slice(str.length-8)
+console.log(str1)
